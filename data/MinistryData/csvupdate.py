78318980af65a1575603c5313ae3449a4ac5dde9
AnthonyClark <anthonyclark.ac@gmail.com>
2014-03-01 16:08:17 -0500

    extracting standard object values

--- data/MinistryData/csvupdate.py
+++ data/MinistryData/csvupdate.py
@@ -8,6 +8,7 @@ headers = f_in.readline().strip().split(',')
 
 
 
+b = {'SO1': "Personnel", 'SO2': "Transportation and Communications", 'SO3': "Information", 'SO4': "Professional and Special Services", 'SO5': "Rentals", 'SO6': "Repair and Maintenance", 'SO7': "Utilities, Materials and Supplies", 'SO8': "Acquisition of Land, Buildings and Works", 'SO9': "Acquistion of Machinery and Equipment", 'SO10': "Transfer Payments", 'SO11': "Public Debt Charges", 'SO12': "Other Subsidies and Payments"}
 for line in f_in:
 	l = line.strip().split(',')
 	for x in range (2, 13):
@@ -15,13 +16,8 @@ for line in f_in:
 		if( int(l[x]) > 0):
 			f_out.write(l[0].strip() + ",")
 			f_out.write(l[1].strip() + ",")
-			f_out.write(headers[x] + ",")
+			f_out.write(b.get(headers[x]) + ",")
 			f_out.write(str(int(l[x]) * 1000) + "\n")
-			#output.append(line[0])
-			#output.append(line[1])
-			#output.append(headers[x])
-			#output.append(str( line[x] )
-			#f_out.write(",".join(output))
 
 f_in.close()
 f_out.close()
